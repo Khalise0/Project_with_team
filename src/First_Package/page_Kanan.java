3e9e7ec1f553b60c52141cede67b74d551accb67
kananmahmudov <kenan_mekanli@yahoo.com>
2024-04-28 16:35:33 -0400

    more practice

--- src/First_Package/page_Kanan.java
+++ src/First_Package/page_Kanan.java
@@ -2,6 +2,7 @@ package First_Package;
 
 public class page_Kanan {
     public static void main(String[] args) {
-        System.out.println("Do your best");
+        System.out.println("Try your best");
     }
+
 }
