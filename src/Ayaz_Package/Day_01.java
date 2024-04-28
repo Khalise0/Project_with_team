a4610aec5b58c14eec2fe41b7284c8ec3f201255
Admin <ayazbagirov89@gmail.com>
2024-04-29 01:22:20 +0400

    update

--- src/Ayaz_Package/Day_01.java
+++ src/Ayaz_Package/Day_01.java
@@ -3,5 +3,6 @@ package Ayaz_Package;
 public class Day_01 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Good Day");
     }
 }
